48119cdab72530679616581d4584bde6210cb96c
Azim Uddin Ahamed <azim@sqbear.com.au>
2022-10-13 08:27:24 +0600

    changes the color

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,3 +1,4 @@
-const WEB__URL = "http://127.0.0.1:8000";
+// const WEB__URL = "http://127.0.0.1:8000";
+const WEB__URL = "http://metaviralexchange.com";
 export const BACKEND_API_URL = WEB__URL + "/api";
 export const BACKEND_WEBSOCKET_URL = WEB__URL;
